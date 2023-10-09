9d1d754c4de4669c90eeade74825e306c9133788
Olena Babko <elena.babko@gmail.com>
2023-10-09 10:57:24 +0300

    Plot contour lines (matplotlib) ((Level Plots))

---
+++ 28_lesson/28_lesson.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as pl
+import numpy as np
+
+
+
+x = np.linspace(-2, 2, 100)
+y = np.linspace(-2, 2, 100)
+
+Z = x[None, :] * np.exp(-x[None, :] ** 2 - y[:, None] ** 2)
+
+X, Y = np.meshgrid(x, y)
+Z = X * np.exp(-X ** 2 - Y ** 2)
+
+'''pl.contour(X, Y, Z, 50, cmap = 'bwr')                   # 20/ 50
+pl.colorbar()'''
+# or
+pl.contourf(X, Y, Z, 50, cmap = 'bwr')
+pl.colorbar()
+
+pl.show()
